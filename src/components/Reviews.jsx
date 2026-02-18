8f17d73d054e0383a2aa8137aa316cddd65d69eb
Jawa <jawasakher@gmail.com>
2026-02-18 23:06:23 +0300

    reveiws.jsx

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,12 @@
+import SectionHeader from "./SectionHeader";
+function Reviews () {
+    return(
+    <section id="reviews">
+        <div className="container">
+            <SectionHeader title={"What Our Students Say"} description={"Hear from our students about their learning experience and success stories."} />
+      </div>
+    </section>
+    );
+
+}
+export default Reviews;
